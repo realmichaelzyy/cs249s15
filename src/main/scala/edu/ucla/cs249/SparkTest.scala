07838260938a5a9732d102e068ed3ce356ff8866
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-05-12 20:42:47 -0700

    test

--- src/main/scala/edu/ucla/cs249/SparkTest.scala
+++ src/main/scala/edu/ucla/cs249/SparkTest.scala
@@ -31,6 +31,7 @@ object SparkTest {
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.parallelize(1 until n, slices).map { i =>
       val zk = new ZooKeeper("54.88.56.9:2181", 5000, null)
+      zk = null
       val x = random * 2 - 1
       val y = random * 2 - 1
       if (x*x + y*y < 1) 1 else 0
