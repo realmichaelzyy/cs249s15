70cbf882f710edf8274fdde40453c953092f11bf
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-05-15 19:58:44 -0700

    ScalaHdfsTest

--- src/main/scala/edu/ucla/cs249/ScalaHdfsTest.scala
+++ src/main/scala/edu/ucla/cs249/ScalaHdfsTest.scala
@@ -4,15 +4,17 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import java.net.URI
+import org.apache.hadoop.fs.permission.FsPermission
 
 object ScalaHdfsTest {
   def main(args: Array[String]) {
     println("----- Scala HDFS Test -----")
     try {
-      val fsuri = URI.create ("hdfs://54.88.56.9:8020/");
-      val conf = new Configuration ();
-      val fs = FileSystem.get(fsuri, conf);
-      val uri = URI.create ("hdfs://54.88.56.9:8020/dev/vardev");
+      val fsuri = URI.create ("hdfs://54.88.56.9:8020/")
+      val conf = new Configuration ()
+      val fs = FileSystem.get(fsuri, conf)
+      val perm = new FsPermission("777")
+      val uri = URI.create ("hdfs://54.88.56.9:8020/dev/vardev")
       fs.mkdirs(new Path(uri))
     } catch {
       case e: Exception => e.printStackTrace()
