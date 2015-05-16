516b30c20d259768d8cde4aacfa16152f9279d32
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-05-15 19:54:00 -0700

    ScalaHdfsTest

--- src/main/scala/edu/ucla/cs249/ScalaHdfsTest.scala
+++ src/main/scala/edu/ucla/cs249/ScalaHdfsTest.scala
@@ -9,9 +9,10 @@ object ScalaHdfsTest {
   def main(args: Array[String]) {
     println("----- Scala HDFS Test -----")
     try {
-      val uri = URI.create ("hdfs://54.88.56.9:8020/vardev");
+      val fsuri = URI.create ("hdfs://54.88.56.9:8020/");
       val conf = new Configuration ();
-      val fs = FileSystem.get(uri, conf);
+      val fs = FileSystem.get(fsuri, conf);
+      val uri = URI.create ("hdfs://54.88.56.9:8020/dev/vardev");
       fs.mkdirs(new Path(uri))
     } catch {
       case e: Exception => e.printStackTrace()
