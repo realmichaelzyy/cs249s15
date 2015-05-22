6ee64020b2f1290f725e8b8b1fba44c4ffadebfd
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-05-22 16:24:40 -0700

    lock and unlock finished

--- src/main/scala/edu/ucla/cs249/SharedVariable.scala
+++ src/main/scala/edu/ucla/cs249/SharedVariable.scala
@@ -15,6 +15,8 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.permission.FsPermission
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
+import org.apache.commons.codec.binary.Hex
+import scala.collection.mutable.HashMap
 
 
 class SharedVariableConfig {
@@ -37,6 +39,8 @@ class SharedVariableConfig {
     builder.setNextVersion(1L)
     builder.clearExistingVersions()
     node_path = zk.create("/sv/sv", builder.build().toByteArray(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL)
+    zk.create(node_path + "/default", null, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    zk.create(node_path + "/dict", null, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
     zk.close()
     
     /* initialize hdfs */
@@ -68,24 +72,98 @@ class SharedVariableConfig {
   }
 }
 
-class SharedVariable {
-
-    def get() = {
-      val zk = new ZooKeeper("54.88.56.9:2181", 5000, null)
-      val stat = new Stat()
-      val varData = zk.getData("/lockdev", false, stat)
-      val bis = new ByteArrayInputStream(varData)
-      val in = new ObjectInputStream(bis)
-      val newobj = in.readObject()
-      newobj
+class SharedVariable (conf: SharedVariableConfig) {
+  /* zookeeper */
+  private var zk: ZooKeeper = null
+  
+  /* default lock */
+  private var hasDefaultLock = false
+  private var defaultLock: DistributedLock = null
+  
+  /* dictionary locks */
+  private var hasDictLock = new HashMap[String, Boolean]
+  private var dictLocks = new HashMap[String, DistributedLock]
+  
+  
+  private def ensureZK {
+    if (zk == null) {
+      zk = new ZooKeeper(conf.zk_connect_string, 5000, null)
     }
+  }
+  
+  private def stringToHex(str: String) = {
+    Hex.encodeHexString(str.getBytes)
+  }
+  
+  def lock {
+    ensureZK
+    val itemPath = conf.node_path + "/default"
+    defaultLock = new DistributedLock(zk, itemPath, "lock")
+    defaultLock.lock()
+    hasDefaultLock = true;
+  }
+  
+  def lockByKey(key: String) {
+    ensureZK
+    val hexKey = stringToHex(key)
+    val itemPath = conf.node_path + "/dict/" + hexKey
     
-    def set(newVal: Any) {
-      val b = new ByteArrayOutputStream()
-      val o = new ObjectOutputStream(b)
-      o.writeObject(newVal)
-      val byteArr = b.toByteArray()
-      val zk = new ZooKeeper("54.88.56.9:2181", 5000, null)
-      zk.setData("/lockdev", byteArr, -1)
+    /* create the node corresponding to key, if not exist */
+    try {
+      zk.create(itemPath, null, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    } catch {
+      case _ : Throwable => 
     }
+   
+    /* create lock */
+    var lock = new DistributedLock(zk, itemPath, "lock")
+    dictLocks.put(hexKey, lock)
+    
+    /* lock it */
+    lock.lock()
+    hasDictLock.put(hexKey, true)
+  }
+  
+  
+  def unlock {
+    if (hasDefaultLock) {
+      ensureZK
+      defaultLock.unlock()
+      defaultLock = null
+      hasDefaultLock = false
+    }
+  }
+  
+  def unlockByKey(key: String) {
+    val hexKey = stringToHex(key)
+    if (hasDictLock.contains(hexKey)) {
+      dictLocks.get(hexKey) match {
+        case Some(lock: DistributedLock) => 
+          lock.unlock()
+          hasDictLock.remove(hexKey)
+          dictLocks.remove(hexKey)
+        case _ =>
+      }
+      
+    }
+  }
+
+  def get() = {
+    val zk = new ZooKeeper("54.88.56.9:2181", 5000, null)
+    val stat = new Stat()
+    val varData = zk.getData("/lockdev", false, stat)
+    val bis = new ByteArrayInputStream(varData)
+    val in = new ObjectInputStream(bis)
+    val newobj = in.readObject()
+    newobj
+  }
+    
+  def set(newVal: Any) {
+    val b = new ByteArrayOutputStream()
+    val o = new ObjectOutputStream(b)
+    o.writeObject(newVal)
+    val byteArr = b.toByteArray()
+    val zk = new ZooKeeper("54.88.56.9:2181", 5000, null)
+    zk.setData("/lockdev", byteArr, -1)
+  }
 }
--- src/main/scala/edu/ucla/cs249/SharedVariableTest.scala
+++ src/main/scala/edu/ucla/cs249/SharedVariableTest.scala
@@ -3,6 +3,18 @@ package edu.ucla.cs249
 import org.apache.zookeeper.ZooKeeper
 import org.apache.zookeeper.ZKUtil
 
+class TestThread(conf: SharedVariableConfig, name: String) extends Runnable {
+  def run {
+    var shared = new SharedVariable(conf)
+    println("before lock " + name)
+    shared.lockByKey("abc" + name)
+    println("with lock " + name)
+    Thread.sleep(1000)
+    shared.unlockByKey("abc" + name)
+    println("after lock " + name)
+  }
+}
+
 object SharedVariableTest {
   def main(args: Array[String]) {
     println("----- Shared Variable Test -----")
@@ -10,7 +22,25 @@ object SharedVariableTest {
     val zk = new ZooKeeper(System.getenv("ZK_CONNECT_STRING"), 5000, null)
     println(conf.node_path)
     println(zk.exists(conf.node_path, false))
+    
+    val thr1 = new Thread(new TestThread(conf, "1"))
+    thr1.start
+    val thr2 = new Thread(new TestThread(conf, "2"))
+    thr2.start
+    thr1.join
+    thr2.join
+    
+    var shared = new SharedVariable(conf)
+//    println("before lock")
+//    shared.lock
+//    println("with lock")
+//    shared.unlock
+//    println("after lock")
+    
+    
+    
     conf.destroy
     println(zk.exists(conf.node_path, false))
+
   }
 }
