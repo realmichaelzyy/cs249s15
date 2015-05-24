5cbc57fe30243e724643bb71a940b218c1f88558
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-05-23 18:38:36 -0700

    merge

--- src/main/java/edu/ucla/cs249/LockTest.java
+++ src/main/java/edu/ucla/cs249/LockTest.java
@@ -56,16 +56,16 @@ public class LockTest {
 //            zk.setData("/lockdev", inodeMsg, stat.getVersion());
             
 			
-			Stat stat = new Stat();
-            byte[] inodeData = zk.getData("/lockdev", false, stat);
-            System.out.println("Version: " + stat.getVersion());
-		    SharedInodeProto.SharedInode inode = SharedInodeProto.SharedInode.parseFrom(inodeData);
-		    System.out.println("nextVersion: " + inode.getNextVersion());
-
-		    for (int i = 0; i < inode.getExistingVersionsCount(); ++i) {
-		        SharedInodeProto.SharedInode.VersionNode vnode = inode.getExistingVersions(i);
-		        System.out.println("existing version " + i + ": " + vnode.getVersion());
-		    }
+//			Stat stat = new Stat();
+//            byte[] inodeData = zk.getData("/lockdev", false, stat);
+//            System.out.println("Version: " + stat.getVersion());
+//		    SharedInodeProto.SharedInode inode = SharedInodeProto.SharedInode.parseFrom(inodeData);
+//		    System.out.println("nextVersion: " + inode.getNextVersion());
+//
+//		    for (int i = 0; i < inode.getExistingVersionsCount(); ++i) {
+//		        SharedInodeProto.SharedInode.VersionNode vnode = inode.getExistingVersions(i);
+//		        System.out.println("existing version " + i + ": " + vnode.getVersion());
+//		    }
 					
 			System.out.println("Before unlock");
 			mutex.unlock();
