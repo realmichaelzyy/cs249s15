7fb8c75085b30fc0951cf741cef1c00a3dcbf643
U-Bochun-PC\Bochun <Bochun@Bochun-PC.(none)>
2015-06-11 19:43:47 -0700

    added Concurrent Write Test

---
+++ src/main/scala/edu/ucla/cs249/ConcurrentWriteTest.scala
@@ -0,0 +1,46 @@
+package edu.ucla.cs249
+
+import org.apache.spark._
+import org.apache.zookeeper._
+import scala.collection.mutable.ArrayBuffer
+import java.util.Calendar
+import java.net.URI
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.permission.FsPermission
+import java.io.ObjectOutputStream
+
+
+object ConcurrentWriteTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("CS 249 Concurrent Write Test")
+
+    val spark = new SparkContext(conf)
+    
+    var beforeParallelize = Calendar.getInstance.getTimeInMillis
+    val count = spark.parallelize(0 until 300).map { i =>
+      var obj_ = new BigObj()
+      for (i <- 0 until 10000000) {
+        obj_.arr.+=(1)
+      }
+      
+      val fsuri = URI.create(System.getenv("HDFS_ADDRESS"))
+      val fsconf = new Configuration()
+      val fs = FileSystem.get(fsuri, fsconf)
+      val keyuri = URI.create(System.getenv("HDFS_ADDRESS") + "/test/" + i)
+      val os = fs.create(new Path(keyuri))
+      fs.setPermission(new Path(keyuri), new FsPermission("777"))
+      val out = new ObjectOutputStream(os)
+      out.writeObject(obj_)
+      out.close()
+      1
+    }.count
+    
+    println("\n-------------\ncount: " + count + "\n---------------")
+    var afterParallelize = Calendar.getInstance.getTimeInMillis
+    println("\n-------------\ntime lapse: " + (afterParallelize-beforeParallelize) + "\n--------------\n")
+
+    spark.stop()
+  }
+}
